1dfe38df4af18d37d4f2310e9e69ace1ba4e07e5
Michael Taylor <mbtaylor@Michaels-MacBook-Pro.local>
2020-04-03 13:13:15 -0400

    Added sorting brews by available/coming soon

--- assets/js/scuffle.js
+++ assets/js/scuffle.js
@@ -5,10 +5,22 @@ $( document ).ready(function() {
 function makeBrew(jsonObject)
 {
 	var html = "";
+
 	html += "<h2>" + jsonObject.name + "</h2>";
-	html += "<h3>" + jsonObject.style + "</h3>";
-	html += "<p>"
-	html += "ABV " + jsonObject.abv + "% - IBU " + jsonObject.ibu;
+    if(jsonObject.style != "");
+	   html += "<h3>" + jsonObject.style + "</h3>";
+
+	html += "<p>";
+
+    if(jsonObject.abv != "")
+	   html += "ABV " + jsonObject.abv + "%";
+
+    if(jsonObject.abv != "" && jsonObject.ibu != "")
+        html += " - "; 
+
+    if(jsonObject.ibu != "")
+        html += "IBU " + jsonObject.ibu;
+    
 	html += "<br />"
 	html += jsonObject.description + "</p>";
 
@@ -18,7 +30,7 @@ function makeBrew(jsonObject)
 function makeBrewList()
 {
 	$.ajax({
-        url: 'assets/data/beerlist.json',
+        url: 'assets/data/beerlist.json?' + Math.random(),
         dataType: 'json',
         type: 'get',
         success: function (data) {
@@ -27,13 +39,21 @@ function makeBrewList()
 
         	for(var i = 0; i < data.beer.length; i++)
         	{
-        		availableNowHTML += makeBrew(data.beer[i]);
-        		availableNowHTML += "<hr></hr>";
+                if(data.beer[i].available == "yes")
+                {
+        		  availableNowHTML += makeBrew(data.beer[i]);
+        		  availableNowHTML += "<hr></hr>";
+                }
+                else if(data.beer[i].comingSoon == "yes")
+                {
+                    comingSoonHTML += makeBrew(data.beer[i]);
+                    comingSoonHTML += "<hr></hr>";
+                }
         	}
         	var html = "<h1>Available Now</h1>";
         	html += "<hr></hr>";
         	html += availableNowHTML;
-        	html += "<br />";
+        	html += "<br /><br /><br />";
         	html += "<h1>Coming Soon</h1>";
         	html += "<hr></hr>";
         	html += comingSoonHTML;
