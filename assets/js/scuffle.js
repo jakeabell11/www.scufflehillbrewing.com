07866d1a9f7a44bf11768be01814add79115cfe5
Michael Taylor <mbtaylor@Michaels-MacBook-Pro.local>
2020-04-03 13:48:18 -0400

    Web content now pulls from text files.

--- assets/js/scuffle.js
+++ assets/js/scuffle.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    makeAllPageInfo();
     makeBrewList();
 });
 
@@ -59,6 +60,101 @@ function makeBrewList()
         	html += comingSoonHTML;
 
         	$("#brews").append(html);
+        }
+    });
+}
+
+function makeAllPageInfo()
+{
+    makeHomeTitle();
+    makeHomeSubTitle();
+    makeCopyright();
+    makeBrewsDescription();
+    makeSupportDescription();
+    makeAboutDescription();
+    makeContactDescription();
+}
+
+function makeHomeTitle()
+{
+    $.ajax({
+        url: 'assets/data/home_title.txt?' + Math.random(),
+        dataType: 'text',
+        type: 'get',
+        success: function (data) {
+            $("#home_title").append(data);
+        }
+    });
+}
+
+function makeHomeSubTitle()
+{
+    $.ajax({
+        url: 'assets/data/home_subtitle.txt?' + Math.random(),
+        dataType: 'text',
+        type: 'get',
+        success: function (data) {
+            $("#home_subtitle").append(data);
+        }
+    });
+}
+
+function makeCopyright()
+{
+    $.ajax({
+        url: 'assets/data/copyright.txt?' + Math.random(),
+        dataType: 'text',
+        type: 'get',
+        success: function (data) {
+            $("#copyright").append(data);
+        }
+    });
+}
+
+function makeBrewsDescription()
+{
+    $.ajax({
+        url: 'assets/data/brews_description.txt?' + Math.random(),
+        dataType: 'text',
+        type: 'get',
+        success: function (data) {
+            $("#brews_description").append(data);
+        }
+    });
+}
+
+function makeSupportDescription()
+{
+    $.ajax({
+        url: 'assets/data/support_description.txt?' + Math.random(),
+        dataType: 'text',
+        type: 'get',
+        success: function (data) {
+            $("#support_description").append(data);
+        }
+    });
+}
+
+function makeAboutDescription()
+{
+    $.ajax({
+        url: 'assets/data/about_description.txt?' + Math.random(),
+        dataType: 'text',
+        type: 'get',
+        success: function (data) {
+            $("#about_description").append(data);
+        }
+    });
+}
+
+function makeContactDescription()
+{
+    $.ajax({
+        url: 'assets/data/contact_description.txt?' + Math.random(),
+        dataType: 'text',
+        type: 'get',
+        success: function (data) {
+            $("#contact_description").append(data);
         }
     });
 }
